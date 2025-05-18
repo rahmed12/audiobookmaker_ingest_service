d8f31eb7ab9e7c64f1bfa591492a10c8abe71a52
rahmed12 <ra56179n@gmail.com>
2025-05-18 19:28:43 +0000

    init project creations

---
+++ src/main/java/com/agent404/audiobook/ingestservice/ingest_service/IngestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agent404.audiobook.ingestservice.ingest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agent404/audiobook/ingestservice/ingest_service/IngestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent404.audiobook.ingestservice.ingest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
